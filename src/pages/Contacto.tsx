97647b675f8b7d6346c54098cfff0827e7f063fd
manuelp1345 <manuelp1345@gmail.com>
2022-07-18 21:39:31 -0400

    finish multi lenguage

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -4,124 +4,98 @@ import GripItem from "../components/grip/GripItem";
 import ContainerCustom from "../components/ContainerCustom";
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
+import { useContext } from "react";
+import { Language } from "../App";
 
 const Contacto = () => {
+  // @ts-ignore
+  const { language } = useContext(Language);
   return (
     <ContainerCustom bg="Contacto">
-      <Box sx={{ paddingTop: "10rem" }}>
-        <GripContainer isFirts>
-          <GripItem>
-            <Box
-              sx={{ height: "40%", borderBottom: "#2f3742 solid 1px" }}
-            ></Box>
-          </GripItem>
-          <GripItem>
-            <Box
-              sx={{ height: "40%", borderBottom: "#2f3742 solid 1px" }}
-            ></Box>
-          </GripItem>
-          <GripItem>
-            <Box
-              sx={{ height: "40%", borderBottom: "#2f3742 solid 1px" }}
-            ></Box>
-          </GripItem>
-          <GripItem>
-            <Box
-              sx={{ height: "40%", borderBottom: "#2f3742 solid 1px" }}
-            ></Box>
-          </GripItem>
-          <GripItem>
-            <Box
-              sx={{ height: "40%", borderBottom: "#2f3742 solid 1px" }}
-            ></Box>
-          </GripItem>
-          <GripItem>
-            <Box
-              sx={{ height: "40%", borderBottom: "#2f3742 solid 1px" }}
-            ></Box>
-          </GripItem>
-          <GripItem isLast></GripItem>
-        </GripContainer>
-        <GripContainer>
-          <GripItem></GripItem>
-          <GripItem>
-            <Box sx={{ height: "250px" }}>
-              <Typography
-                sx={{
-                  color: "white",
-                  fontWeight: "bold",
-                  marginLeft: 2,
-                  marginTop: "1rem",
-                  marginBottom: "1rem",
-                }}
-              >
-                Contactos
-              </Typography>
-            </Box>
-          </GripItem>
-          <GripItem>
-            <Box
-              sx={{
-                height: "40%",
-                borderBottom: "#2f3742 solid 1px",
-              }}
-            >
-              <Typography
-                sx={{
-                  color: "white",
-                  marginLeft: 2,
-                  position: "absolute",
-                  top: "25%",
-                  left: "20%",
-                  width: "60%",
-                }}
-              >
-                <span style={{ fontWeight: "bold" }}>
-                  Mayfair Berkeley Square&nbsp;
-                </span>
-                Berkeley Square Berkeley Square House, 2nd Floor Berkeley
-                Square, London, W1J6BD, GBR
-              </Typography>
-              <Typography
-                sx={{
-                  color: "white",
-                  marginLeft: 2,
-                  position: "absolute",
-                  top: "50%",
-                  left: "20%",
-                  width: "60%",
-                }}
-              >
-                <span style={{ fontWeight: "bold" }}>Telefono:&nbsp;</span>
-                +44 7939 980432
-              </Typography>
-              <Typography
-                sx={{
-                  color: "white",
-                  marginLeft: 2,
-                  position: "absolute",
-                  top: "75%",
-                  left: "20%",
-                  width: "60%",
-                }}
-              >
-                <span style={{ fontWeight: "bold" }}>Correo:&nbsp;</span>
-                admin@soltechgr.com
-              </Typography>
-            </Box>
-          </GripItem>
-          <GripItem></GripItem>
-          <GripItem></GripItem>
-          <GripItem isLast></GripItem>
-        </GripContainer>
-        <GripContainer spaceMid>
-          <GripItem></GripItem>
-          <GripItem></GripItem>
-          <GripItem></GripItem>
-          <GripItem></GripItem>
-          <GripItem></GripItem>
-          <GripItem isLast></GripItem>
-        </GripContainer>
+      <Box
+        sx={{
+          paddingTop: "10rem",
+          background:
+            "linear-gradient(0deg, rgba(23,32,44,1) 3%, rgba(23,32,44,0.4) 100%)",
+          height: "100%",
+          px: "20%",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100%",
+            width: "100%",
+            borderBottom: "#2f3742 solid 1px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "white",
+              fontWeight: "bold",
+              marginLeft: 2,
+              marginBottom: "1rem",
+              fontSize: "2.5rem",
+            }}
+          >
+            {language === "english" ? "Contacts" : "Contactos"}
+          </Typography>
+          <Typography
+            sx={{
+              color: "white",
+              marginLeft: 2,
+              width: "100%",
+              marginBottom: "1.5rem",
+            }}
+          >
+            <span style={{ fontWeight: "bold" }}>
+              Mayfair Berkeley Square&nbsp;
+            </span>
+            Berkeley Square Berkeley Square House, 2nd Floor Berkeley Square,
+            London, W1J6BD, GBR
+          </Typography>
+          <Typography
+            sx={{
+              color: "white",
+              marginLeft: 2,
+              width: "100%",
+              marginBottom: "1.5rem",
+            }}
+          >
+            <span style={{ fontWeight: "bold" }}>Colombia&nbsp;</span>
+            Carrera 40 5A 271 Sur Torre 2-204 Canaguay/Amarilo Villavicencio,
+            Meta, Colombia
+          </Typography>
+          <Typography
+            sx={{
+              color: "white",
+              marginLeft: 2,
+              width: "100%",
+              marginBottom: "1.5rem",
+            }}
+          >
+            <span style={{ fontWeight: "bold" }}>
+              {language === "english" ? (
+                <>Phone:&nbsp;</>
+              ) : (
+                <>Telefono:&nbsp;</>
+              )}
+            </span>
+            +44 7939 980432 (UK) &nbsp;/&nbsp; +57 320-8223709 (Colombia)
+          </Typography>
+          <Typography
+            sx={{
+              color: "white",
+              marginLeft: 2,
+              width: "100%",
+              marginBottom: "1.5rem",
+            }}
+          >
+            <span style={{ fontWeight: "bold" }}>
+              {language === "english" ? <>Email:&nbsp;</> : <>Correo:&nbsp;</>}
+            </span>
+            admin@soltechgr.com &nbsp;/&nbsp; admin@soltechgroup.net
+          </Typography>
+        </Box>
       </Box>
     </ContainerCustom>
   );
